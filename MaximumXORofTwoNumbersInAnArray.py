82f22d43406a55362b819166cd1ee03c8e6110bc
timowang1991 <timowang1991@yahoo.com.tw>
2016-11-13 19:03:57 +0800

    Maximum XOR of Two Numbers in an Array

---
+++ MaximumXORofTwoNumbersInAnArray.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findMaximumXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        mask = 0
+        for x in xrange(31, -1, -1):
+            mask += 1 << x
+            prefixSet = set([n & mask for n in nums])
+            tmp = ans | 1 << x
+            for prefix in prefixSet:
+                if tmp ^ prefix in prefixSet:
+                    ans = tmp
+                    break
+        return ans
